f91e593bfcd862d1b80ac6e70d45ddad1b0003d3
oriionn <xemilien67@gmail.com>
2023-08-06 17:11:03 +0200

    Patch ESLINT

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,29 @@
-import {QuectoRequester} from "./request";
-import FormData from "form-data";
+import { QuectoRequester } from './request'
+import FormData from 'form-data'
 
 export class Quecto {
-    public readonly instanceUrl: string;
-    public readonly requester: QuectoRequester;
-    private as: any;
+  public readonly instanceUrl: string
+  public readonly requester: QuectoRequester
 
-    public constructor(instanceUrl: string) {
-        this.requester = new QuectoRequester(instanceUrl);
-        this.instanceUrl = this.requester.instanceUrl;
-    }
+  public constructor (instanceUrl: string) {
+    this.requester = new QuectoRequester(instanceUrl)
+    this.instanceUrl = this.requester.instanceUrl
+  }
 
-    public async shortUrl(url: string): Promise<Response> {
-        let formData = new FormData();
-        formData.append("link", url);
-        return (await this.requester.post("shorten", formData));
-    }
+  public async shortUrl (url: string): Promise<Response> {
+    const formData = new FormData()
+    formData.append('link', url)
+    return (await this.requester.post('shorten', formData))
+  }
 
-    public async unshortUrl(url: string): Promise<Response> {
-        // @ts-ignore
-        let code: String = url.split("/").pop();
-        return (await this.requester.get("s/" + code));
-    }
+  public async unshortUrl (url: string): Promise<Response> {
+    // @ts-expect-error
+    const code: String = url.split('/').pop()
+    return (await this.requester.get(`s/${code.toString()}`))
+  }
 
-    public async isValidInstance(): Promise<Boolean> {
-        // @ts-ignore
-        return (await this.requester.get("quectoCheck").then((res: Response) => res.response.status === 200).catch((res) => !res.response));
-    }
+  public async isValidInstance (): Promise<Boolean> {
+    // @ts-expect-error
+    return (await this.requester.get('quectoCheck').then((res: Response) => res.response.status === 200).catch((res) => (res.response === undefined)))
+  }
 }
